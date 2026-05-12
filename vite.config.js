c6c5a92f1505bf0443e66bda1c290380dea6dd0f
teresa <teresa.almeidacastro@gmail.com>
2026-05-12 14:03:54 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/eu/',
-});
+  // O nome do teu repositório no GitHub. Altera se não for "eu"
+  base: '/eu/', 
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      // Isto é o que diz ao telemóvel "Sou uma App!"
+      manifest: {
+        name: 'JEEP EDUCA+',
+        short_name: 'JEEP',
+        description: 'Aplicação de Gestão JEEP EDUCA+',
+        theme_color: '#1e293b', // A cor do teu Soft Dark Mode
+        background_color: '#1e293b',
+        display: 'standalone', // Faz com que abra sem a barra de endereço do browser
+        icons: [
+          {
+            src: 'logo.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'logo.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
+})
