a1e1e22093de0f9f0048a4857d0698a4e5ee82c1
teresa <teresa.almeidacastro@gmail.com>
2026-05-13 15:36:57 +0100

    Update ForumPost.jsx

--- src/tabs/forum/ForumPost.jsx
+++ src/tabs/forum/ForumPost.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import { doc, updateDoc, deleteDoc, addDoc, collection, increment, arrayUnion } from "firebase/firestore";
-import { db } from "../firebase.js";
-import { CARD, CYN, INP } from "../theme.jsx";
-import { nowLabel, FORUM_REACTIONS } from "../data.js";
+import { collection, addDoc, doc, updateDoc, increment, arrayUnion } from "firebase/firestore";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { db, storage } from "../../firebase.js";
+import { CARD, SL, CYN, INP } from "../../theme.jsx";
+import { nowLabel } from "../../data.js";
 
 export default function ForumPost({ post, user, canalAtivo }) {
   const [responderA, setResponderA] = useState(false);
